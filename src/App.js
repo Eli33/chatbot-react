cb8428776a604093e00d3ed3aed4f7d0fc8d0e2e
Eliana Queiroz <eli.alencarqueiroz@gmail.com>
2018-11-30 08:56:08 -0200

    projeto react

--- src/App.js
+++ src/App.js
@@ -6,25 +6,21 @@ import { Button } from 'reactstrap';
 
 class App extends Component {
     render() {
-        return ( < div className = "App" >
-            <
-            header className = "App-header" >
-            <
-            img src = { logo }
+        return ( 
+        <div className = "App">
+            <header className = "App-header">
+            <img src = { logo }
             className = "App-logo"
-            alt = "logo" / >
-            <
-            p >
-            Edit < code > src / App.js < /code> and save to reload. </p >
-            <
-            Button color = "danger" > Danger! < /Button>  <
-            a className = "App-link"
+            alt = "logo"/>
+            <p> Edit <code > src / App.js </code> and save to reload. </p>
+            <Button color = "danger" > Danger! </Button>
+            <a className = "App-link"
             href = "https://reactjs.org"
             target = "_blank"
             rel = "noopener noreferrer" >
-            Learn React < /a> <
-            /header> <
-            /div>
+            Learn React </a> 
+            </header> 
+            </div>
         );
     }
 }
